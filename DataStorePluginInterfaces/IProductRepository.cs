28c6b0e0a4d1f7ea35d2eb8eaec9326ffc68baf1
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2024-07-26 22:02:59 -0400

    feat: Create IProductRepository

--- DataStorePluginInterfaces/IProductRepository.cs
+++ DataStorePluginInterfaces/IProductRepository.cs
@@ -5,8 +5,9 @@ namespace UseCases.DataStorePluginInterfaces;
 public interface IProductRepository
 {
     void AddProduct(Product product);
-    IEnumerable<Product> GetProducts();
+    IEnumerable<Product> GetProducts(bool loadCategory);
     Product? GetProductById(int productId, bool loadCategory);
     void UpdateProduct(int productId, Product product);
     void DeleteProduct(int productId);
+    IEnumerable<Product> GetProductsByCategoryId(int categoryId);
 }
