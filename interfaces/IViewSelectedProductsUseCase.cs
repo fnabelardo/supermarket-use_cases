c5f99f91a7f080c8d45ab4fb3874e0c6ac6c1423
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2024-07-29 18:38:33 -0400

    feat: Set loadCategory parameter default false

--- interfaces/IViewSelectedProductsUseCase.cs
+++ interfaces/IViewSelectedProductsUseCase.cs
@@ -4,5 +4,5 @@ namespace UseCases.ProductsUseCases;
 
 public interface IViewSelectedProductsUseCase
 {
-    Product? Execute(int productId, bool loadCategory);
+    Product? Execute(int productId, bool loadCategory = false);
 }
